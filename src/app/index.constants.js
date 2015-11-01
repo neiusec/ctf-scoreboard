6ae1f5e950ad1042be6324e7a415fc9dc99787ea
Mike Abreu <dev.mikeabreu@gmail.com>
2015-11-01 14:44:14 -0600

    v0.0.0 - Initial Commit - Clean generator-gulp-angular from yeoman

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('ctfScoreboard')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.route.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ctfScoreboard')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
